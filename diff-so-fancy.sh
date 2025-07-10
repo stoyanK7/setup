ce01a52c67e264624f41dd2d3ff28f54a72784c9
Stoyan <stoyank127@gmail.com>
2025-07-10 16:21:40 +0300

    add forgotten sudo

--- diff-so-fancy.sh
+++ diff-so-fancy.sh
@@ -2,8 +2,8 @@
 
 echo "Installing diff-so-fancy..."
 
-curl -o /usr/local/bin/diff-so-fancy https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.4/diff-so-fancy
-chmod +x /usr/local/bin/diff-so-fancy
+sudo curl -L -o /usr/local/bin/diff-so-fancy https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.4/diff-so-fancy
+sudo chmod +x /usr/local/bin/diff-so-fancy
 
 echo "Configuring diff-so-fancy..."
 git config --global core.pager "diff-so-fancy | less --tabs=4 -RF"
